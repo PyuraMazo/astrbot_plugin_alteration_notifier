6557b9956796388607c3c2ffe175cab2696032a6
PyuraMazo <1605025385@qq.com>
2026-02-26 21:35:06 +0800

    v1.0.1

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class AlterationNotifierPlugin(Star):
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-        await self.astrbot_loaded()
+        await self.save_external_activated_plugins()
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
@@ -63,65 +63,61 @@ class AlterationNotifierPlugin(Star):
                         names |= AlterationNotifierPlugin.collect_command_names(check)
                 self.activated_plugins[cmd.name] = names
 
+
     async def notify(self, msg: str):
         for group in self.role_range:
             await self.ctx.send_message(
                 self.UNIFIED_MSG_ORIGIN_PREFIX + group, MessageChain().message(msg)
             )
 
-    @filter.on_astrbot_loaded()
-    async def astrbot_loaded(self):
-        """延迟初始化"""
-
-        await self.save_external_activated_plugins()
 
     @filter.on_plugin_loaded()
     async def plugin_loaded(self, metadata: StarMetadata):
         """监测新插件启用、加载"""
 
-        if self.activated_plugins:
-            names = set()
-            all_commands = await command_management.list_commands()
+        names = set()
+        all_commands = await command_management.list_commands()
 
-            plugin = metadata.name
-            for check in all_commands:
-                if plugin == check["plugin"]:
-                    added = AlterationNotifierPlugin.collect_command_names(check)
-                    names |= added
-                    cache = self.unloaded_cache.keys()
-                    for one in added:
-                        if one in cache:
-                            del self.unloaded_cache[one]
-            self.activated_plugins[plugin] = names
+        plugin = metadata.name
+        for check in all_commands:
+            if plugin == check["plugin"]:
+                added = AlterationNotifierPlugin.collect_command_names(check)
+                names |= added
+                cache = self.unloaded_cache
+                for one in added:
+                    if one in cache:
+                        del self.unloaded_cache[one]
+        self.activated_plugins[plugin] = names
 
-            if not self.monitor_self and plugin == self.SELF:
-                return
-            await self.notify(f"新增插件：{plugin}")
+        if not self.monitor_self and plugin == self.SELF:
+            return
+        await self.notify(f"新增插件：{plugin}")
 
     @filter.on_plugin_unloaded()
     async def plugin_unloaded(self, metadata: StarMetadata):
         """监测插件禁用、卸载"""
 
-        if self.activated_plugins:
-            plugin = metadata.name
-            now = int(datetime.now().timestamp())
-            names = self.activated_plugins.pop(plugin)
-            for name in names:
-                self.unloaded_cache[name] = now
+        plugin = metadata.name
+        now = int(datetime.now().timestamp())
+        names = self.activated_plugins.pop(plugin)
+        for name in names:
+            self.unloaded_cache[name] = now
 
-            if not self.monitor_self and plugin == self.SELF:
-                return
-            await self.notify(f"移除插件：{plugin}")
+        if not self.monitor_self and plugin == self.SELF:
+            return
+        await self.notify(f"移除插件：{plugin}")
 
     @filter.event_message_type(filter.EventMessageType.ALL, priority=-10)
     async def monitor(self, event: AstrMessageEvent):
         """监控唤醒机器人的消息"""
-        msg = event.message_str
-        if event._has_send_oper or not msg:
+
+        if event._has_send_oper or not event.is_at_or_wake_command:
             return
 
+        pure_text = event.message_str
+        print(pure_text)
         for name in self.unloaded_cache:
-            if msg.startswith(name):
+            if pure_text.startswith(name):
                 yield event.plain_result(
                     f"指令【{name}】已经于【{datetime.fromtimestamp(self.unloaded_cache[name])}】被移除了！"
                 )
@@ -131,6 +127,6 @@ class AlterationNotifierPlugin(Star):
     async def collect_group(self, event: AstrMessageEvent):
         """自动收集群聊id"""
 
-        id = event.get_group_id()
-        if self.exclude_mode and id not in self.group:
-            self.role_range.add(id)
+        group_id = event.get_group_id()
+        if self.exclude_mode and group_id not in self.group:
+            self.role_range.add(group_id)
